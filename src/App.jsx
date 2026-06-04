4bd0241cbaf1b5f97384537197eaacecdba54724
Akequreru <161937569+Akequreru@users.noreply.github.com>
2026-06-04 14:50:07 +0900

    ver2.7.1

--- src/App.jsx
+++ src/App.jsx
@@ -73,10 +73,10 @@ export default function PoseSwordWeb() {
   const [isCopied, setIsCopied] = useState(false);
 
   const { unityProvider, sendMessage, isLoaded } = useUnityContext({
-    loaderUrl: "/POSE_SWORD_Unity/Builds/ver2.7/Build/ver2.7.loader.js",
-    dataUrl: "/POSE_SWORD_Unity/Builds/ver2.7/Build/ver2.7.data",
-    frameworkUrl: "/POSE_SWORD_Unity/Builds/ver2.7/Build/ver2.7.framework.js",
-    codeUrl: "/POSE_SWORD_Unity/Builds/ver2.7/Build/ver2.7.wasm",
+    loaderUrl: "/POSE_SWORD_Unity/Builds/ver2.7.1/Build/ver2.7.1.loader.js",
+    dataUrl: "/POSE_SWORD_Unity/Builds/ver2.7.1/Build/ver2.7.1.data",
+    frameworkUrl: "/POSE_SWORD_Unity/Builds/ver2.7.1/Build/ver2.7.1.framework.js",
+    codeUrl: "/POSE_SWORD_Unity/Builds/ver2.7.1/Build/ver2.7.1.wasm",
   });
 
   const pendingBattleRef = useRef(null);
